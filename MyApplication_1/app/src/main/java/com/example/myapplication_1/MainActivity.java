bb240d0a212d759902505e06efcb84ea26595e5b
161207 <AAASSS@gmail.com>
2019-05-23 10:15:43 +0300

    Final_Laba_4

--- MyApplication_1/app/src/main/java/com/example/myapplication_1/MainActivity.java
+++ MyApplication_1/app/src/main/java/com/example/myapplication_1/MainActivity.java
@@ -1,70 +1,65 @@
 package com.example.myapplication_1;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,
-        AdapterView.OnItemClickListener,
-        AdapterView.OnItemLongClickListener{
+public class MainActivity extends Activity {
 
     TextView tv;
     Button button1;
-    EditText edit;
-
-    ListView listView;
-    ArrayList nameList = new ArrayList();
-    ArrayAdapter adapter;
+    Button button2;
+    Button button3;
+    int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.text_view);
-        tv.setText(getString(R.string.hellojava));
+        tv = (TextView) findViewById(R.id.text_view);
+        button1 = (Button) findViewById(R.id.btn1);
+        button2 = (Button) findViewById(R.id.btn2);
+        button3 = (Button) findViewById(R.id.cln);
 
-        button1 = findViewById(R.id.btn1);
-        button1.setOnClickListener(this);
-        edit = findViewById(R.id.edtx);
 
-        listView = findViewById(R.id.listview);
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nameList);
+        View.OnClickListener plus = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cnt += 1;
+                tv.setText("Кнопка нажата " + Integer.toString(cnt) + " раз");
+                if(cnt%10 <= 4 && cnt%10 > 1  && cnt != 12 && cnt != 13 && cnt != 14) {
+                    tv.append("a");
+                }
+            }
+        };
 
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-        listView.setOnItemLongClickListener(this);
+
+        View.OnClickListener minus = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cnt -= 1;
+                tv.setText("Кнопка нажата " + Integer.toString(cnt) + " раз");
+                if(cnt%10 <= 4 && cnt%10 > 1  && cnt != 12 && cnt != 13 && cnt != 14) {
+                    tv.append("a");
+                }
+            }
+        };
+
+
+        View.OnClickListener clean = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Hello From Java!");
+                cnt = 0;
+            }
+        };
+        button1.setOnClickListener(plus);
+        button2.setOnClickListener(minus);
+        button3.setOnClickListener(clean);
     }
 
-    @Override
-    public void onClick(View v) {
-        //tv.setText(getString(R.string.Text_updated));
-        if(!nameList.contains(edit.getText().toString())) {
-            nameList.add(edit.getText().toString());
-            Collections.sort(nameList);
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        tv.setText(nameList.get(position).toString());
-    }
-
-
-    @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        nameList.remove(nameList.get(position));
-        adapter.notifyDataSetChanged();
-        return false;
-    }
 }
